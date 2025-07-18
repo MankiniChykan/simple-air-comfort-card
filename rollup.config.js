d71b066e23f9087f5c948112b287c921f2656b2b
MankiniChykan <101962955+MankiniChykan@users.noreply.github.com>
2025-07-18 17:36:44 +0800

    Create rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,16 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
+
+export default {
+  input: 'src/simple-air-comfort-card.js',
+  output: {
+    file: 'dist/simple-air-comfort-card.js',
+    format: 'esm'
+  },
+  plugins: [
+    resolve(),
+    commonjs(),
+    terser()
+  ]
+};
