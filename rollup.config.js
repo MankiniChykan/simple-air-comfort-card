54ab9ddde01cba4d5014389f549484152a136e4f
MankiniChykan <101962955+MankiniChykan@users.noreply.github.com>
2025-07-20 16:50:28 +0800

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import { terser } from 'rollup-plugin-terser';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import terser from '@rollup/plugin-terser';
 
 export default {
   input: 'src/simple-air-comfort-card.js',
