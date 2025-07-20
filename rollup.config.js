fd91f9cf7471640331cbcaccbb22ba59625471a9
MankiniChykan <101962955+MankiniChykan@users.noreply.github.com>
2025-07-20 17:10:26 +0800

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,16 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import { terser } from '@rollup/plugin-terser'; // FIXED: use named import
+import terser from '@rollup/plugin-terser';
 
 export default {
   input: 'src/simple-air-comfort-card.js',
   output: {
     file: 'dist/simple-air-comfort-card.js',
-    format: 'esm',
+    format: 'esm'
   },
   plugins: [
     resolve(),
     commonjs(),
-    terser(), // call the named export
-  ],
+    terser()
+  ]
 };
