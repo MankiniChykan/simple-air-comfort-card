fd0d462a66665f1c50acb627c0bd27d70295edcc
MankiniChykan <101962955+MankiniChykan@users.noreply.github.com>
2025-08-15 22:19:29 +0000

    modified:   rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import replace from '@rollup/plugin-replace';
 import terser from '@rollup/plugin-terser';
+import gzipPlugin from 'rollup-plugin-gzip';
 
 export default {
   input: 'src/simple-air-comfort-card.js',
