7ec51aea749ee9f2606bfda1fdb183fec52904e9
MankiniChykan <101962955+MankiniChykan@users.noreply.github.com>
2025-08-18 09:06:01 +0000

    modified:   src/simple-air-comfort-card.js

--- src/simple-air-comfort-card.js
+++ src/simple-air-comfort-card.js
@@ -156,8 +156,6 @@ class SimpleAirComfortCard extends LitElement {
       transform: translate(-50%, -50%);
       width: 45%;
       height: 45%;
-      min-width: 8em;
-      min-height: 8em;
     }
 
     /* Axis labels placed around the dial rim */
