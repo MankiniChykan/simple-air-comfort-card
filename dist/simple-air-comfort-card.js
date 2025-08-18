b743b3753f6fb7ba69529f56a1e1b0b082377ea4
MankiniChykan <101962955+MankiniChykan@users.noreply.github.com>
2025-08-18 09:02:37 +0000

    chore(release): v1.0.78

--- dist/simple-air-comfort-card.js
+++ dist/simple-air-comfort-card.js
@@ -48,7 +48,7 @@ const v=globalThis,x=v.trustedTypes,A=x?x.createPolicy("lit-html",{createHTML:t=
       margin: auto;       /* center both axes */
       width: auto%;
       max-width: 100%;
-      height: 100;       /* let aspect-ratio drive the height */
+      height: 100%;       /* let aspect-ratio drive the height */
       max-height: 100%;
       aspect-ratio: 1 / 1;/* true square without the padding-top hack */
       box-sizing: border-box;
