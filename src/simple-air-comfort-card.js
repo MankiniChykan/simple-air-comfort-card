28d3e622a4678b80fbb98952913e8fae0b0cf0e1
MankiniChykan <101962955+MankiniChykan@users.noreply.github.com>
2025-08-20 03:36:34 +0000

    modified:   src/simple-air-comfort-card.js

--- src/simple-air-comfort-card.js
+++ src/simple-air-comfort-card.js
@@ -102,8 +102,8 @@ class SimpleAirComfortCard extends LitElement {
       display: block;
       margin-top: 0.1rem;
     }
-    .tl{ left:8%;  top:18%;  transform:translate(0,-50%); text-align:left; }
-    .tr{ right:8%; top:18%;  transform:translate(0,-50%); text-align:right; }
+    .tl{ left:8%;  top:20%;  transform:translate(0,-50%); text-align:left; }
+    .tr{ right:8%; top:20%;  transform:translate(0,-50%); text-align:right; }
     .bl{ left:8%;  bottom:3%; transform:translate(0,0);   text-align:left; }
     .br{ right:8%; bottom:3%; transform:translate(0,0);   text-align:right; }
 
