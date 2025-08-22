1facb0d7f49a13d72373226915a0fb007371ad30
MankiniChykan <101962955+MankiniChykan@users.noreply.github.com>
2025-08-22 00:17:30 +0000

    modified:   src/simple-air-comfort-card.js

--- src/simple-air-comfort-card.js
+++ src/simple-air-comfort-card.js
@@ -1004,9 +1004,9 @@ SimpleAirComfortCard.prototype.getGridOptions = function () {
     rows: 4,      // ~ 4 * 56px + gaps managed by HA
 
     // Reasonable bounds so it still looks good when users resize:
-    min_columns: 3,
+    min_columns: 6,
     max_columns: 12,
-    min_rows: 3,
+    min_rows: 4,
     max_rows: 6,
   };
 };
