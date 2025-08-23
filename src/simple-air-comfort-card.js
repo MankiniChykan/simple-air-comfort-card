4265b9ea0d82afc5d24aae77c624d1d7bfa099c9
MankiniChykan <101962955+MankiniChykan@users.noreply.github.com>
2025-08-23 04:02:13 +0000

    modified:   src/simple-air-comfort-card.js

--- src/simple-air-comfort-card.js
+++ src/simple-air-comfort-card.js
@@ -1334,10 +1334,10 @@ SimpleAirComfortCard.prototype.getCardSize = function () {
 SimpleAirComfortCard.prototype.getGridOptions = function () {
   return {
     columns: 6,       // default width (use multiples of 3 for tidy grid)
-    rows: 4,     // height adjusts to content
-    min_columns: 3,   // don’t let it shrink too small
-    max_columns: 12,  // can span full width
+    rows: "auto",     // height adjusts to content
+    min_columns: 4,   // don’t let it shrink too small
+    max_columns: 4,  // can span full width
     min_rows: 1,
-    max_rows: 8,
+    max_rows: 1,
   };
 };
